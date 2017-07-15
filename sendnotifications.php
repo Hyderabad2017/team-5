278f351a31a6cfa7ca0d5ad1da515e0f7daf4fab
Saikeerthi123 <ykeerthana1996@gmail.com>
2017-07-15 20:52:03 +0000

    sendnoti updated

--- sendnotifications.php
+++ sendnotifications.php
@@ -1,19 +1,24 @@
 <html>
-<body 
-<?php
-// Required if your environment does not handle autoloading
-require __DIR__ . '/vendor/autoload.php';
+<body background="./assets/img/backgrounds/1.jpg">
+<center>
+<p></p>
+<center>
+</body>
+</html> 
+	<?php
+	// Required if your environment does not handle autoloading
+	require __DIR__ . '/vendor/autoload.php';
 
-// Use the REST API Client to make requests to the Twilio REST API
-use Twilio\Rest\Client;
+	// Use the REST API Client to make requests to the Twilio REST API
+	use Twilio\Rest\Client;
 
-// Your Account SID and Auth Token from twilio.com/console
-$sid = 'AC1bd9c3e05e0c85a69529296f36b4cfdc';
-$token = '2344a89b5a2a7a1d07611e1ef17c8a8b';
-$client = new Client($sid, $token);
+	// Your Account SID and Auth Token from twilio.com/console
+	$sid = 'AC1bd9c3e05e0c85a69529296f36b4cfdc';
+	$token = '2344a89b5a2a7a1d07611e1ef17c8a8b';
+	$client = new Client($sid, $token);
 
-// Use the client to do fun stuff like send text messages!
-$client->messages->create(
+	// Use the client to do fun stuff like send text messages!
+	$client->messages->create(
     // the number you'd like to send the message to
     '+917680974517',
     array(
